84239588f3b7d8ea07b0a9b5ba57a0a08a1dce7e
Hannah Smith <hchristinasmith@gmail.com>
2025-06-26 09:38:18 +1200

    created migrations

---
+++ server/db/migrations/20250625213638_expense_types.js
@@ -0,0 +1,15 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  return knex.schema.createTable('fruit', (table) => {
+    table.increments('id')
+    table.string('name')
+    table.string('owner').defaultTo(null)
+  })
+}
+
+export async function down(knex) {
+  return knex.schema.dropTable('fruit')
+}
