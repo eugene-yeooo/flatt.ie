90678ff8643a299bd6a7f4d1dfbcac44927b65ea
Hannah Smith <hchristinasmith@gmail.com>
2025-06-26 09:40:13 +1200

    flatties migration' '

--- server/db/migrations/20250625213525_flattie.js
+++ server/db/migrations/20250625213525_flattie.js
@@ -2,14 +2,17 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+export async function up(knex) {
+  return knex.schema.createTable('fruit', (table) => {
+    table.increments('id').primary()
+    table.string('name').notNullable()
+    table.decimal('credit').defaultTo(0)
+    table.decimal('debt').defaultTo(0)
+    table.decimal('balance').defaultTo(0)
+  })
+}
+
+export async function down(knex) {
+  return knex.schema.dropTable('flatties')
+}
