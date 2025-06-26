719dfa35bc94839e29bf74543b300cd26f36c22d
Hannah Smith <hchristinasmith@gmail.com>
2025-06-26 17:05:33 +1200

    joins

--- server/db/payment.ts
+++ server/db/payment.ts
@@ -1,5 +1,14 @@
 import connection from './connection.ts'
 
 export async function getAllPayments() {
-  return connection('payment').select('*')
+  return connection('payment')
+    .join('bill', 'payment.bill_id', 'bill.id')
+    .join('flatmate', 'payment.flatmate_id', 'flatmate.id')
+    .select(
+      'payment.*',
+      'bill.title as billTitle',
+      'bill.due_date',
+      'flatmate.name as flatmateName',
+      'flatmate.profile_photo',
+    )
 }
