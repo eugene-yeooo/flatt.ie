6584d799a74f619d120cbdebeda5586539f03f8a
Eugene Yeo <eugeneyeoooo@gmail.com>
2025-06-26 14:43:49 +1200

    create getAllBills express route and db function; tested with postman

--- server/server.ts
+++ server/server.ts
@@ -1,19 +1,17 @@
 import express from 'express'
 import * as Path from 'node:path'
 
-
 import flattiesRoutes from './routes/flatties.ts'
-
 import expenseRoutes from './routes/expense.ts'
+import billRoutes from './routes/bill.ts'
 
 const server = express()
 
 server.use(express.json())
 
-
 server.use('/api/v1/flatties', flattiesRoutes)
 server.use('/api/v1/expense', expenseRoutes)
-
+server.use('/api/v1/bill', billRoutes)
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
