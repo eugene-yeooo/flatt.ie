f597b5b6075c90c10e4f24c9d74129f5b6ef90bd
Hannah Smith <hchristinasmith@gmail.com>
2025-06-30 10:36:06 +1200

    home changes

--- client/components/Home/Home.tsx
+++ client/components/Home/Home.tsx
@@ -8,27 +8,36 @@ export default function Home() {
   if (isLoading) return <p>Loading summary...</p>
   if (isError) return <p>Failed to load summary.</p>
 
+  //count num of flatities by flattiename for each payment
   const numOfFlatties = new Set(payments.map((p) => p.flattieName)).size
 
+  //date related constants
   const today = new Date()
   const daysRange = 30
   const msInDay = 1000 * 60 * 60 * 24
 
+  //ensure all dates are parsed as date objects
   function parseDate(date: string | Date): Date {
     return date instanceof Date ? date : new Date(date)
   }
 
+  //store most recent bill entry by IDfor each billTitle within date range
   const recentBillMap = new Map<
     string,
     { billTotal: number; latestId: number; dueDate: string | Date }
   >()
 
+  //loop for payments
   payments.forEach((p) => {
+    //ensure valid data
     if (p.billTitle && typeof p.billTotal === 'number' && p.dueDate) {
       const due = parseDate(p.dueDate)
+      //calculate number of days between due date and today
       const diffDays = (due.getTime() - today.getTime()) / msInDay
+      //only include bills within +/- 30 days of today
       if (diffDays >= -daysRange && diffDays <= daysRange) {
         const existing = recentBillMap.get(p.billTitle)
+        //if no existing bill OR current bill has newer ID, update more recent versions to map
         if (!existing || p.id > existing.latestId) {
           recentBillMap.set(p.billTitle, {
             billTotal: p.billTotal,
