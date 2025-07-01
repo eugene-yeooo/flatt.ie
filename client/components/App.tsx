36f18f7faae3ac0683eaf092764be56970e95df2
Hannah Smith <hchristinasmith@gmail.com>
2025-07-01 20:39:49 +1200

    create user

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 import Dashboard from './Dashboard/Dashboard'
 import Bills from './Bills/Bills'
 import Expenses from './Expenses/Expenses'
@@ -8,9 +8,31 @@ import Flatties from './Flatties/Flatties'
 import Front from './Front'
 import { useAuth0 } from '@auth0/auth0-react'
 import Home from './Home/Home'
-
+import Register from './Register/CreateUser'
+import { useEffect } from 'react'
+import { useUser } from '../../client/hooks/useUser'
+import { useLocation } from 'react-router-dom'
+import Profile from './Profile/Profile'
 export default function App() {
-  const { isAuthenticated } = useAuth0()
+  const { isAuthenticated, getAccessTokenSilently } = useAuth0()
+  const navigate = useNavigate()
+  const user = useUser()
+  const location = useLocation()
+  const hideNav = location.pathname === '/register'
+
+  //useUser hook in compoents to know roles
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      const getToken = async () => {
+        const token = await getAccessTokenSilently()
+        console.log(token)
+      }
+      getToken()
+    }
+
+    if (!user.data && isAuthenticated) navigate('/register')
+  }, [user.data, navigate, isAuthenticated])
 
   if (!isAuthenticated) {
     return <Front />
@@ -51,20 +73,22 @@ export default function App() {
         }}
         role="banner"
       >
-        <div className="mx-auto flex max-w-6xl items-center justify-between gap-6">
-          <Link
-            to="/flattie"
-            style={{ backgroundColor: 'var(--background)' }}
-            className="rounded-xl border-8 border-white px-3 py-2 text-4xl font-extrabold tracking-tight text-[var(--primary)] shadow drop-shadow-[1px_1px_2px_var(--border)] backdrop-blur-sm "
-            aria-label="Flatt.ie Home"
-          >
-            flatt.ie
-          </Link>
+        {!hideNav && (
+          <div className="mx-auto flex max-w-6xl items-center justify-between gap-6">
+            <Link
+              to="/flattie"
+              style={{ backgroundColor: 'var(--background)' }}
+              className="rounded-xl border-8 border-white px-3 py-2 text-4xl font-extrabold tracking-tight text-[var(--primary)] shadow drop-shadow-[1px_1px_2px_var(--border)] backdrop-blur-sm "
+              aria-label="Flatt.ie Home"
+            >
+              flatt.ie
+            </Link>
 
-          <nav className="flex-1">
-            <Navigation />
-          </nav>
-        </div>
+            <nav className="flex-1">
+              <Navigation />
+            </nav>
+          </div>
+        )}
       </header>
 
       <main className="mx-auto max-w-6xl rounded-xl bg-white p-6 shadow">
@@ -76,6 +100,8 @@ export default function App() {
           <Route path="/bills" element={<Bills />} />
           <Route path="/expense" element={<Expenses />} />
           <Route path="/report" element={<Report data={sampleData} />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </main>
     </div>
